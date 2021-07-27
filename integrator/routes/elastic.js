3e611dc4d464372c3e6942d28a3ed111be19422d
Angelo Rafael <angelorpt@gmail.com>
2021-07-26 22:55:52 -0400

    wip | Tentando conectar fazer requisição do Node para o Laravel

--- integrator/routes/elastic.js
+++ integrator/routes/elastic.js
@@ -1,16 +1,39 @@
 var express = require("express");
 const axios = require("axios");
+var http = require("http");
 
 var router = express.Router();
 
+// http.get(configuracoes, function (res) {
+//   //console.log(res.statusCode);
+//   res.on("data", function (body) {
+//     console.log("Corpo: " + body);
+//   });
+// });
+
 /* GET home page. */
 router.get("/", async function (req, res, next) {
+  //   axios
+  //     .get("http://app:8100")
+  //     .then(function (response) {
+  //       res.json({ response: response });
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       res.json({ error: error });
+  //     });
+
   try {
-    const response = await axios.get("http://app:80/api/noticias/1");
-    // const response = await axios.get("http://elastic:9200/");
-    res.json({ response: response });
+    // const response = await axios.get(
+    //   "https://jsonplaceholder.typicode.com/todos"
+    // );
+    const response = await axios.get("http://nginx/api/noticias/1");
+    //     // const response = await fetch("http://app:8100/api/noticias/1");
+    //     // waits until the request completes...
+    //     console.log(response);
+    res.json({ response });
   } catch (error) {
-    res.json({ error: error });
+    res.json({ error, a: true });
   }
 });
 
